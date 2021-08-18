b80ec083cb1d7b5a5f12ed75306cbe18c13d9a74
ejscribner <ejscribner@gmail.com>
2021-08-18 15:43:44 -0700

    updating print statements

--- util/ensureIndexes.js
+++ util/ensureIndexes.js
@@ -8,6 +8,7 @@ export const ensureIndexes = async(COUCHBASE_BUCKET) => {
     console.log("ensureIdx 3");
     const bucketIndex = `CREATE PRIMARY INDEX ON ${COUCHBASE_BUCKET}`
     const collectionIndex = `CREATE PRIMARY INDEX ON default:${COUCHBASE_BUCKET}._default.profile;`
+    console.log("ensureIdx 3.5");
     await cluster.query(bucketIndex)
     console.log("ensureIdx 4");
     await cluster.query(collectionIndex)
@@ -25,4 +26,5 @@ export const ensureIndexes = async(COUCHBASE_BUCKET) => {
       console.error(err)
     }
   }
+  console.log("ensureIdx 9");
 }
