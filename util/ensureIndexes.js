e236df78575233d2e5e8260c6ecc023e5b671418
ejscribner <ejscribner@gmail.com>
2022-04-06 12:15:52 -0700

    cleanup `ensureIndexes`

--- util/ensureIndexes.js
+++ util/ensureIndexes.js
@@ -1,17 +1,30 @@
 import {connectToDatabase} from "./couchbase";
 const couchbase = require("couchbase")
 
-export const ensureIndexes = async(COUCHBASE_BUCKET) => {
-  let {cluster} = await connectToDatabase();
+export const ensureIndexes = async() => {
+  let { cluster } = await connectToDatabase();
+  const COUCHBASE_BUCKET = process.env.COUCHBASE_BUCKET
+
+  const bucketIndex = `CREATE PRIMARY INDEX ON ${COUCHBASE_BUCKET}`
+  const collectionIndex = `CREATE PRIMARY INDEX ON default:${COUCHBASE_BUCKET}._default.profile;`
+
   try {
-    const bucketIndex = `CREATE PRIMARY INDEX ON ${COUCHBASE_BUCKET}`
-    const collectionIndex = `CREATE PRIMARY INDEX ON default:${COUCHBASE_BUCKET}._default.profile;`
     await cluster.query(bucketIndex)
-    await cluster.query(collectionIndex)
-    console.log(`Index Creation: SUCCESS`)
+    console.log(`Bucket Index Creation: SUCCESS`)
   } catch (err) {
     if (err instanceof couchbase.IndexExistsError) {
-      console.info('Index Creation: Indexes Already Exists')
+      console.info('Bucket Index Creation: Index Already Exists')
+    } else {
+      console.error(err)
+    }
+  }
+
+  try {
+    await cluster.query(collectionIndex)
+    console.log(`Collection Index Creation: SUCCESS`)
+  } catch (err) {
+    if (err instanceof couchbase.IndexExistsError) {
+      console.info('Collection Index Creation: Index Already Exists')
     } else {
       console.error(err)
     }
