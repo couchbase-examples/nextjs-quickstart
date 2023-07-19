bdbc6a67d50d58a5b9a8e4b88ff5716830d10d76
ejscribner <elliot.scribner@couchbase.com>
2023-07-19 16:11:21 -0700

    Fixed

--- components/content-panel/gradient/Gradient.js
+++ components/content-panel/gradient/Gradient.js
@@ -5,7 +5,7 @@ const Gradient = ({ firstName, lastName }) => {
 
   const stringToValue = (str) => {
     let value = 0;
-    for (let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str?.length; i++) {
       value += str.charCodeAt(i);
     }
     return value;
